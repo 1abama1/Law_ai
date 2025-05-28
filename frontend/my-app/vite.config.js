c697c682d7fb45c2ae2db827e1d150f07481bd28
Ali-Nur <alinursagynbaev31@gmail.com>
2025-05-28 16:50:33 +0600

    final commit 4.4

--- frontend/my-app/vite.config.js
+++ frontend/my-app/vite.config.js
@@ -23,6 +23,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });

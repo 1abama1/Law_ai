d49748f8c8126edbb6b37c392293ea872807b63f
Ali-Nur <alinursagynbaev31@gmail.com>
2025-05-28 16:27:23 +0600

    final commit 3.3

--- frontend/my-app/deploy.sh
+++ frontend/my-app/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # Устанавливаем переменные окружения
+export NODE_ENV=production
 export VITE_API_URL=https://polniy-bankich.onrender.com:10000
 export VITE_AI_SERVICE_URL=https://ai-service-ziam.onrender.com
 export VITE_API_SOCKET_URL=https://polniy-bankich.onrender.com:10000
 
-# Собираем и запускаем приложение
-npm run build && npm run preview -- --host 0.0.0.0 --port 10001 
+# Собираем и запускаем приложение в production-режиме
+NODE_ENV=production npm run build
+npm run preview -- --host 0.0.0.0 --port 10001 

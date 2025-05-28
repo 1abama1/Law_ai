1ac4330a7e1b955b41b7ea06ba8965d699b61625
Ali-Nur <alinursagynbaev31@gmail.com>
2025-05-28 13:51:31 +0600

    initial commit

--- src/main/java/org/example/bank/Repository/LoanStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.bank.Repository;
-
-import org.example.bank.Entity.LoanStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
- 
-public interface LoanStatusRepository extends JpaRepository<LoanStatus, Long> {
-    LoanStatus findByName(String name);
-} 

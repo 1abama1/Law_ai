af54c769d9b53f3e8237b2a1b53060a81213bc98
Ali-Nur <alinursagynbaev31@gmail.com>
2025-05-28 16:15:14 +0600

    final commit 3

--- frontend/my-app/deploy.sh
+++ frontend/my-app/deploy.sh
@@ -6,4 +6,4 @@ export VITE_AI_SERVICE_URL=https://ai-service-ziam.onrender.com
 export VITE_API_SOCKET_URL=https://polniy-bankich.onrender.com:10000
 
 # Собираем и запускаем приложение
-npm run build && npm run preview -- --host 0.0.0.0 --port 5173 
+npm run build && npm run preview -- --host 0.0.0.0 --port 10001 
--- frontend/my-app/vite.config.js
+++ frontend/my-app/vite.config.js
@@ -6,13 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 10001,
     strictPort: true,
     open: true
   },
   preview: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 10001,
     strictPort: true
   },
   resolve: {
